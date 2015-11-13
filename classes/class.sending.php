d8a1b2389d4c3427174eb3e440d387b8ddd811e3
Hudson Atwell <hudson@inboundnow.com>
2015-11-13 11:39:29 -0600

    Fixing broken templating by ignoring links that are not actual URLs: https://github.com/inboundnow/inbound-mailer/issues/61#issuecomment-156308409

--- classes/class.sending.php
+++ classes/class.sending.php
@@ -117,8 +117,8 @@ class Inbound_Mail_Daemon {
 		/* Iterate over the extracted links and display their URLs */
 		foreach ($links[1] as $link){
 
-			/* Do not modify unsubscribe links */
-			if ( strstr( $link , '?token=') ) {
+			/* Do not modify unsubscribe links or non links */
+			if ( strstr( $link , '?token=') || !strstr($link,'//:') ) {
 				continue;
 			}
 
@@ -532,7 +532,7 @@ class Inbound_Mail_Daemon {
 
 		/* add tracking params to links */
 		$html = self::rebuild_links( $html );
-
+		error_log($html);
 		return $html;
 
 	}
