834ee4d11941ac6bd45896dbc65df5ea16f6acab
GITUSER <hudson@inboundnow.com>
2017-03-07 18:14:48 -0600

    Updating email queue table with rule id and job id

--- classes/class.activation.database-routines.php
+++ classes/class.activation.database-routines.php
@@ -56,6 +56,8 @@ class Inbound_Mailer_Activation_Update_Routines {
 			`email_id` mediumint(9) NOT NULL,
 			`variation_id` mediumint(9) NOT NULL,
 			`lead_id` mediumint(9) NOT NULL,
+			`job_id` mediumint(9) NOT NULL,
+			`rule_id` mediumint(9) NOT NULL,
 			`token` tinytext NOT NULL,
 			`type` tinytext NOT NULL,
 			`tokens` text NOT NULL,
@@ -74,7 +76,7 @@ class Inbound_Mailer_Activation_Update_Routines {
 	* @migration-type: db modification
 	* @mirgration: creates wp_inbound_email_queue table
 	*/
-	public static function alert_inbound_email_queue() {
+	public static function alter_inbound_email_queue_add_tokens() {
 		global $wpdb;
 		require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
 
@@ -85,10 +87,31 @@ class Inbound_Mailer_Activation_Update_Routines {
 		/* add ip field if does not exist */
 		$row = $wpdb->get_results(  "SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE table_name = '{$table_name}' AND column_name = 'tokens'"  );
 		if(empty($row)){
-			$wpdb->get_results( "ALTER TABLE {$table_name} ADD `tokens` text NOT NULL" );
+			$wpdb->get_results( "ALTER TABLE {$table_name} ADD `tokens` mediumint(9) NOT NULL" );
 		}
 
-		dbDelta( $sql );
+	}
+
+	/**
+	 * Alerter table
+	* @migration-type: db modification
+	* @mirgration: creates wp_inbound_email_queue table
+	*/
+	public static function alter_inbound_email_queue_add_job_id_rule_id() {
+		global $wpdb;
+		require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
+
+		$table_name = $wpdb->prefix . "inbound_email_queue";
+
+		$charset_collate = $wpdb->get_charset_collate();
+
+		/* add ip field if does not exist */
+		$row = $wpdb->get_results(  "SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE table_name = '{$table_name}' AND column_name = 'job_id'"  );
+		if(empty($row)){
+			$wpdb->get_results( "ALTER TABLE {$table_name} ADD `job_id` mediumint(9) NOT NULL" );
+			$wpdb->get_results( "ALTER TABLE {$table_name} ADD `rule_id` mediumint(9) NOT NULL" );
+		}
+
 	}
 
 
